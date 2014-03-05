12f97d0e6fe1cdc0f1d46ee66fed5da4d82d273c
Huibvdvosse <huibvandevosse@gmail.com>
2014-03-05 11:23:50 +0100

    A fresh restart.

--- ProjectX/ProjectX/Properties/AssemblyInfo.cs
+++ ProjectX/ProjectX/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication2")]
+[assembly: AssemblyTitle("ProjectX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication2")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("ProjectX")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dda8a602-f32b-43cc-bae7-b69169bcd2df")]
+[assembly: Guid("965bd281-8cce-4017-b198-edb880619afc")]
 
 // Version information for an assembly consists of the following four values:
 //
